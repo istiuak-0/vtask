4346b4c37f1b05ebd62558316a59de48d71be164
istiuak <istiuak.0@gmail.com>
2026-04-21 22:21:49 +0600

    add initial setup

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: "./src/core.ts",
+  entry: ["./src/index.ts"],
 
   format: ["esm"],
   dts: { resolve: true },
-  sourcemap: true,
+  sourcemap: false,
   clean: true,
   splitting: true,
   minify: false,
